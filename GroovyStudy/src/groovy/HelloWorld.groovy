7f36565ea884ad990580b91d0281ef19fa72be68
QiFeiLXH <1005488340@qq.com>
2019-09-29 16:26:05 +0800

    1

--- GroovyStudy/src/groovy/HelloWorld.groovy
+++ GroovyStudy/src/groovy/HelloWorld.groovy
@@ -4,7 +4,7 @@ package groovy
  * @BelongsProject: ProjectManagement* @BelongsPackage: groovy* @Author: QiFei* @CreateTime: 2019-09-29 15:52
  * @Description: ${Description}
  */
-class LearnGroovy {
+class HelloWorld {
     static void main(args){
         println("Hello World!")
     }
