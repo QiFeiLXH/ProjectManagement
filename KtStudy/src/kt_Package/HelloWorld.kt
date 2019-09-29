4e02970785d67e22eed4ddbf5e454b8589f7fb50
QiFeiLXH <1005488340@qq.com>
2019-09-29 15:32:55 +0800

    提交

---
+++ KtStudy/src/kt_Package/HelloWorld.kt
@@ -0,0 +1,13 @@
+package kt_Package
+
+/**
+ *@BelongsProject: ProjectManagement
+ *@BelongsPackage: kotlin
+ *@Author: QiFei
+ *@CreateTime: 2019-09-29 15:02
+ *@Description:
+ */
+
+fun main(args: Array<String>) {
+    print("Hello World!")
+}
